7d5b093cd1dddb25248f98147bb2d290078da84d
Salah-Eddin El Hasnaoui <140462983+sel-hasn@users.noreply.github.com>
2026-08-03 14:03:49 +0100

    Add insert_data.sh script for database population

---
+++ insert_data.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+while IFS=, read -r year round winner opponent winner_goals opponent_goals
+do
+  if [[ $year != 'year' ]]
+  then
+    # Insert winner team if it does not exist
+    $PSQL "INSERT INTO teams(name) VALUES('$winner') ON CONFLICT (name) DO NOTHING"
+
+    # Insert opponent team if it does not exist
+    $PSQL "INSERT INTO teams(name) VALUES('$opponent') ON CONFLICT (name) DO NOTHING"
+
+    # Get team IDs
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$winner'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$opponent'")
+
+    # Insert game
+    echo "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+    VALUES($year, '$round', $WINNER_ID, $OPPONENT_ID, $winner_goals, $opponent_goals);")"
+  fi
+done < games.csv
